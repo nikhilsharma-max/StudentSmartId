9418e0aeb32efb06fb094a9c110ab50b3c2f9692
Nikhil Sharma <nikhilsharma745408@gmail.com>
2026-06-22 11:42:20 +0530

    migrate email to resend

--- StudentSmartIdproject-2/Backend/Services/Email.js
+++ StudentSmartIdproject-2/Backend/Services/Email.js
@@ -1,51 +1,31 @@
-const nodemailer = require('nodemailer');
 const path = require("path");
+const { Resend } = require("resend");
 
 require("dotenv").config({
     path: path.resolve(__dirname, "../.env")
 });
 
-// const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     host: 'smtp.gmail.com',
-//     port: 465,
-//     secure: true,   
-//     auth: {
-//         user: process.env.HOST,
-//         pass: process.env.APP_PASSWORD
-//     }
-// });
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4, // force IPv4
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.APP_PASSWORD
-    }
-});
-transporter.verify((error, success) => {
-    if (error) {
-        console.error("SMTP Verify Error:", error);
-    } else {
-        console.log("SMTP Server Ready");
-    }
-});
-async function sendEmail(to, subject, text){
-    console.log("Sending email to:", to);
-    const mailOptions = {
-        from: process.env.HOST,
-        to,
-        subject,
-        html: `<p>${text}</p>`
-    };
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+async function sendEmail(to, subject, html) {
     try {
-        await transporter.sendMail(mailOptions);
-        console.log('Email sent successfully');
+        console.log(`Sending email to: ${to}`);
+
+        const data = await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to,
+            subject,
+            html
+        });
+
+        console.log("Email sent successfully");
+        console.log(data);
+
+        return data;
     } catch (error) {
-        console.error('Error sending email:', error);
+        console.error("Error sending email:", error);
+        throw error;
     }
-};  
+}
 
 module.exports = { sendEmail };
